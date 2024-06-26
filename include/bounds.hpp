4b1fb8a5cd0f89beeb165f8b442461583a536d44
ygleolee <ygleolee@gmail.com>
2024-06-25 22:57:17 -0400

    bounds

--- include/bounds.hpp
+++ include/bounds.hpp
@@ -5,7 +5,7 @@
 
 // LP paper
 double freq_UB(dist_t& dist, int64_t G, double err); // Coro 4
-double samp_LB(dist_t& dist, int64_t G, int64_t d, double err); // Thm 5
+double samp_LB(dist_t& dist, int64_t G, double err); // Thm 5
 double extended_LB(); // Coro 7
 double prior_LB(dist_t& dist, int64_t G, int64_t j, double err1, double err2); // Thm 9
 // double best_prior_LB(dist_t& dist, int64_t G, double err1, double err2); // Thm 9 with different j, return best
--- src/bounds.cpp
+++ src/bounds.cpp
@@ -10,7 +10,13 @@ double freq_UB(dist_t& dist, int64_t G, double err) { // Coro 4
   return std::min(((double) top_G_freq) / ((double) dist.N) + eps, 1.0);
 }
 
-double samp_LB(dist_t& dist, int64_t G, int64_t d, double err) { // Thm 5
+double samp_LB(dist_t& dist, int64_t G, double err) { // Thm 5
+  if (dist.D2.empty()) {
+    std::cerr << "must partition before calculating sampling LB" << std::endl;
+    return -1.0;
+  }
+
+  double t = sqrtl(-log(err) / (2.0 * dist.D2.size()));
 }
 
 double prior_LB(dist_t& dist, int64_t G, int64_t j, double err1, double err2) { // Thm 9
@@ -18,6 +24,34 @@ double prior_LB(dist_t& dist, int64_t G, int64_t j, double err1, double err2) { 
     std::cerr << "No L value satisfy the constraints on the parameters" << std::endl;
     return -1.0;
   }
+
+  if (j < 2) {
+    std::cerr << "Invalid j value. j must be greater than or equal to 2." << std::endl;
+    return -1.0;
+  }
+
+  double L = ((double) G) / ((double) dist.N);
+  double temp = (double) dist.N;
+  for (int i=0; i<j-1; ++i) {
+    temp /= ((j-1) * L);
+  }
+
+  int lo = 0, hi = dist.freqcount.size() - 1, mid;
+  while (lo != hi) {
+    mid = (lo + hi + 1) >> 1;
+    if (dist.freqcount[mid].first >= j) {
+      lo = mid;
+    }
+    else {
+      hi = mid - 1;
+    }
+  }
+
+  double f_SL = ((double) dist.preftotal[lo]) / ((double) dist.N) - temp;
+  double t = sqrtl(-dist.N * j * j * log(err1) / 2);
+  double eps = sqrtl(-log(err2) / (2.0 * dist.N));
+
+  return std::max(f_SL - t/dist.N - eps, 0.0);
 }
 
 double extended_LB() { // Coro 7
@@ -28,6 +62,7 @@ double extended_LB() { // Coro 7
 
 
 // PIN paper
+
 double new_LB_model() { // Thm 3
 }
 
@@ -35,6 +70,5 @@ double new_LB_samp() { // Coro 4
 }
 
 double new_UB(dist_t& dist, int64_t G, double err) { // Thm 2
-  // auto it = lower_bound();
-  int64_t F = 0;
+  int64_t F = most_frequent(dist, G);
 }
